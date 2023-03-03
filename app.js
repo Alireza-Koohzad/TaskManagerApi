bd2a392a61fe41738e7242287969ebc91946ff54
Alireza-Koohzad <alirezakhd2705@gmail.com>
2023-03-03 13:38:24 +0330

    add some packages

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 const express = require("express");
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
-// app.all("*", (req, res, next) => {
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
 
-// });
+//connect to DB
+mongoose.connect(
+  `mongodb+srv://alirezakoohzad:alireza894@cluster0.k8p9o.mongodb.net/task-manager`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 
 
-dotenv.config({ path: '.env' });
+const userRoutes = require("./routes/userRoutes");
+const errorController = require("./controllers/errorController");
+
+app.use("/api/v1/users", userRoutes);
+
+app.all("*", (req, res, next) => {
+  throw new Error("این ادرس یافت نشد");
+});
+
+app.use(errorController)
+dotenv.config({ path: ".env" });
 const port = process.env.SERVER_PORT;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -10,15 +11,34 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  password : {
-    type :String ,
-    required : true
+  password: {
+    type: String,
+    required: true,
+    select: false,
   },
-  age : {
-    type : Number
-  }
+  confirmPassword: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
 });
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+  next();
+});
 
-const User = mongoose.model('User' , userSchema);
+userSchema.methods.comparePassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
