93ef94f797767e6a4275eb71479d12f347f04dc0
Alireza-Koohzad <alirezakhd2705@gmail.com>
2023-03-05 11:41:38 +0330

    update routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const {
   loginValidation,
   forgetValidation,
   resetValidation,
+  updatePasswordValidation,
 } = require("../validations/authValidation");
 const express = require("express");
 
@@ -12,12 +13,16 @@ const {
   login,
   forgetPassword,
   resetPassword,
+  updatePassword,
 } = require("../controllers/authController");
+const protect = require("../middlewares/protectMiddleware");
 
 router.post("/signup", signupValidation, signup);
 router.post("/login", loginValidation, login);
 
 router.post("/forgetPassword", forgetValidation, forgetPassword);
 router.post("/resetPassword/:token", resetValidation, resetPassword);
+
+router.patch("/updatePassword", protect , updatePasswordValidation , updatePassword);
 
 module.exports = router;
