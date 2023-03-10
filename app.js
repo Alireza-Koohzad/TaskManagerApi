6fcd1e5f9d01914cc919761ec4d401bae4d6b688
Alireza-Koohzad <alirezakhd2705@gmail.com>
2023-03-10 16:35:42 +0330

    config multer

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const multer = require("multer");
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '/images'));
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(multer({storage : fileStorage, fileFilter : fileFilter}).single('image'))
 app.use(cors());
+// app.use("/images", express.static(path.join(__dirname, "images")));
 
 //connect to DB
 mongoose.connect(
@@ -24,8 +48,6 @@ db.once("open", function () {
   console.log("Connected successfully");
 });
 
-
-
 const userRoutes = require("./routes/userRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 
@@ -38,7 +60,7 @@ app.all("*", (req, res, next) => {
   throw new Error("این ادرس یافت نشد");
 });
 
-app.use(errorController)
+app.use(errorController);
 dotenv.config({ path: ".env" });
 const port = process.env.SERVER_PORT;
 const server = app.listen(port, () => {
